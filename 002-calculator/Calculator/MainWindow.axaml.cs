b3e9b2e917271c920b32ec714bb97491b6360d87
llarndorfer <l.larndorfer@students.htl-leonding.ac.at>
2026-09-25 10:21:02 +0200

    chore: change from mvvm to basic app

---
+++ 002-calculator/Calculator/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Calculator;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
--- 002-calculator/Calculator/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Calculator.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
