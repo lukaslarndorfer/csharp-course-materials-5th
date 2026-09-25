612e08bd971759a9165698f7e29e4d00e500346b
llarndorfer <l.larndorfer@students.htl-leonding.ac.at>
2026-09-25 18:06:02 +0200

    feat: implement basic calculator UI and input functionality

--- 002-calculator/Calculator/MainWindow.axaml.cs
+++ 002-calculator/Calculator/MainWindow.axaml.cs
@@ -1,11 +1,31 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Calculator;
 
 public partial class MainWindow : Window
 {
+
+    private string _calculation = string.Empty; 
     public MainWindow()
     {
         InitializeComponent();
     }
+
+    private void OnButtonClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button { Tag: not null } button)
+        {
+            var input =  button.Tag.ToString();
+            _calculation = CalculationLogic.HandleCalculationInput(_calculation, input ?? string.Empty);
+            UpdateCalculation();
+        }
+    }
+
+    private void UpdateCalculation()
+    {
+        Calculation.Text =  _calculation;
+    }
+    
+    
 }
